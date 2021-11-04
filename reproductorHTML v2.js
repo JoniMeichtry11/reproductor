7a1e8f4eb645b704305f304ae61239dd344594d3
Jonathan Meichtry <joni.meichtry@gmail.com>
2021-11-04 09:07:58 -0300

    funcion jquery para carga

---
+++ reproductorHTML v2.js
@@ -0,0 +1,17 @@
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play");
+
+
+$(document).ready(function() {
+    playPause.addEventListener("click", () => {
+      if (audio.paused || audio.ended) {
+        playPause.querySelector(".pause-btn").classList.toggle("hide");
+        playPause.querySelector(".play-btn").classList.toggle("hide");
+        audio.play();
+      } else {
+        audio.pause();
+        playPause.querySelector(".pause-btn").classList.toggle("hide");
+        playPause.querySelector(".play-btn").classList.toggle("hide");
+      }
+    });
+});
